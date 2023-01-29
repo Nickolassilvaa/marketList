6b83300dae128ef21b38a66088490264c1eca868
nrp-silva <nickolascaragua@gmail.com>
2023-01-28 23:50:48 -0300

    Correções e alteração de design

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ export function App() {
   const [list, setList] = useState<listProps[]>([]);
   const [updateList, setUpdateList] = useState(false);
   const [count, setCount] = useState(0);
+  const [countItems, setCountItems] = useState(0);
   const [formState, setFormState] = useState<form>({
     title: "",
     price: "",
@@ -33,10 +34,15 @@ export function App() {
           setList(response.data);
 
           let newCount = 0;
+          let newCountItems = 0;
+
           response.data.forEach((item: listProps) => {
             newCount += Number(item.price) * Number(item.amount);
+            newCountItems += 1;
           });
+
           setCount(newCount);
+          setCountItems(newCountItems);
         })
         .catch((e) => console.log(e));
     } catch (error) {
@@ -92,74 +98,95 @@ export function App() {
   return (
     <>
       <div className="flex justify-center items-center w-screen h-[20vh]">
-        <h1 className="text-white font-bold text-2xl">Lista supermercado</h1>
+        <h1 className="text-white font-bold text-2xl uppercase">
+          Lista supermercado
+        </h1>
       </div>
 
-      <div className="w-screen h-auto ">
+      <div className="w-screen h-auto px-4">
         {/* Form */}
-        <div className="max-w-[780px] w-full h-auto flex items-center justify-center gap-2 mx-auto mt-[-20px]">
-          <input
-            type="text"
-            placeholder="Adicionar um item ..."
-            className=" outline-none px-4 py-3 rounded w-full"
-            value={formState.title}
-            onChange={(event) =>
-              setFormState({
-                ...formState,
-                title: event.currentTarget?.value,
-              })
-            }
-            id="item"
-          />
+        <form>
+          <div className="max-w-[780px] w-full h-auto flex items-center justify-center gap-2 mx-auto mt-[-20px]">
+            <input
+              type="text"
+              placeholder="Adicionar um item ..."
+              className=" outline-none px-4 py-3 rounded w-full"
+              value={formState.title}
+              onChange={(event) =>
+                setFormState({
+                  ...formState,
+                  title: event.currentTarget?.value,
+                })
+              }
+              id="item"
+            />
 
-          <input
-            type="text"
-            placeholder="R$ 0.00"
-            onInput={(event) => {
-              event.currentTarget.value = event.currentTarget.value
-                .replace(/[^0-9.]/g, "")
-                .replace(/(\..*?)\..*/g, "$1");
-            }}
-            className="rounded px-2 py-3 w-24 text-center"
-            value={formState.price}
-            onChange={(event) =>
-              setFormState({
-                ...formState,
-                price: event.currentTarget?.value,
-              })
-            }
-            id="price"
-          />
-          <input
-            type="text"
-            onInput={(event) => {
-              event.currentTarget.value = event.currentTarget.value
-                .replace(/[^0-9.]/g, "")
-                .replace(/(\..*?)\..*/g, "$1");
-            }}
-            placeholder="0"
-            className="rounded px-2 py-3 w-20 text-center"
-            value={formState.amount}
-            onChange={(event) =>
-              setFormState({
-                ...formState,
-                amount: event.currentTarget?.value,
-              })
-            }
-            id="amount"
-          />
-          <button
-            className="px-2 py-3 text-white bg-purple-100 rounded disabled:cursor-not-allowed disabled:bg-purple-300 disabled:text-gray-500"
-            onClick={addItems}
-            disabled={!formState.amount || !formState.title || !formState.price}
-          >
-            adicionar
-          </button>
-        </div>
+            <input
+              type="text"
+              placeholder="R$ 0.00"
+              onInput={(event) => {
+                event.currentTarget.value = event.currentTarget.value
+                  .replace(/[^0-9.]/g, "")
+                  .replace(/(\..*?)\..*/g, "$1");
+              }}
+              className="rounded px-2 py-3 text-center w-20"
+              value={formState.price}
+              onChange={(event) =>
+                setFormState({
+                  ...formState,
+                  price: event.currentTarget?.value,
+                })
+              }
+              id="price"
+            />
+
+            <input
+              type="text"
+              onInput={(event) => {
+                event.currentTarget.value = event.currentTarget.value
+                  .replace(/[^0-9.]/g, "")
+                  .replace(/(\..*?)\..*/g, "$1");
+              }}
+              placeholder="0"
+              className="rounded px-2 py-3 w-14 text-center"
+              value={formState.amount}
+              onChange={(event) =>
+                setFormState({
+                  ...formState,
+                  amount: event.currentTarget?.value,
+                })
+              }
+              id="amount"
+            />
+            <button
+              type="submit"
+              onSubmit={(event) => {
+                event.preventDefault();
+                addItems();
+              }}
+              className="px-2 py-3 text-white bg-green-100 hover:bg-green-200 transition-all rounded disabled:cursor-not-allowed disabled:bg-green-200 disabled:text-gray-500"
+              onClick={addItems}
+              disabled={
+                !formState.amount || !formState.title || !formState.price
+              }
+            >
+              adicionar
+            </button>
+          </div>
+        </form>
 
         {/* Content */}
         <div className="max-w-[780px] w-full h-auto flex flex-col items-center justify-center gap-2 mx-auto mt-16">
-          {count}
+          <div className="flex justify-between items-center w-full border-b border-dashed pb-2 mb-4">
+            <p className="ext-base font-bold">
+              Total itens:{" "}
+              <span className="bg-gray-600 p-1 rounded-full">{countItems}</span>
+            </p>
+            <p className="text-base font-bold">
+              Total:{" "}
+              <span className="text-3xl ml-2 text-green-100">R$ {count}</span>
+            </p>
+          </div>
           {list.map((item) => {
             return (
               <div className="w-full flex gap-2" key={item.id}>
